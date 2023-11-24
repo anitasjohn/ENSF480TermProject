0d95bed2898b154d6ece33ba570ea814bcd43516
kdafrose <katherine.tubang@ucalgary.ca>
2023-11-24 15:24:33 -0700

    Added implementation for all classes

---
+++ Email.java
@@ -0,0 +1,22 @@
+/**
+ * File: Email.java
+ * Created By: Group 11 (L03 B03)
+ * Submission: November 29, 2023
+ */
+
+public class Email{
+    private String emailAddress;
+
+    Email(String email){
+        this.emailAddress = email;
+    }
+
+    public String getEmail(){
+        return this.emailAddress;
+    }
+
+    public void setEmail(String emailString){
+        this.emailAddress = emailString;
+    }
+
+}
---
+++ Name.java
@@ -0,0 +1,35 @@
+/**
+ * File: Name.java
+ * Created By: Group 11 (L03 B03)
+ * Submission: November 29, 2023
+ */
+
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    Name(String first, String last){
+        this.firstName = first;
+        this.lastName = last;
+    }
+
+    public void setFirstName(String first){
+        this.firstName = first;
+    }
+
+    public void setLastName(String last){
+        this.lastName = last;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public String getFullName(){
+        return this.firstName + " " + lastName + "\n"; 
+    }
+}
