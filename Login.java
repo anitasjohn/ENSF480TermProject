234aaefc1195bd56cbad54b4b03e76b0b2102eaf
kdafrose <katherine.tubang@ucalgary.ca>
2023-11-25 19:44:26 -0700

    Created login class

---
+++ Login.java
@@ -0,0 +1,14 @@
+/**
+ * File: Login.java
+ * Created By: Group 11 (L03 B03)
+ * Submission: November 29, 2023
+ */
+
+public class Login{
+    // Singleton Pattern
+
+    private Login(){
+        // initialize for ArrayLists for passwords for all types of users
+    }
+
+}
