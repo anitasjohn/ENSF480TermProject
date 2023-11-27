d3a4093860502f42e05497ea59e4d6b4e6589c67
janpetallo <101074771+janpetallo@users.noreply.github.com>
2023-11-26 19:45:57 -0700

    Edited Flight.java and Populate Flight database

--- src/Flight.java
+++ src/Flight.java
@@ -6,43 +6,66 @@
 
 public class Flight {
     // Private instance variables
-    private String destination;     // place of destination
-    private Address location;        // address of airport 
+    private String fromCity;     // place of departure
+    private String toCity;      // place of departure
+    //private Address location;        // address of airport 
+    private String fromAirport;
+    private String toAirport;
     private String duration;        // duration of flight
     private int flightNumber;       // flight id
-    private String takeoffNumber;   // gate number at airport 
+    //private String takeoffNumber;   // gate number at airport 
     private String timeOfFlight;    // time of the flight
+    private String price;
 
     // Constructors
     public Flight() {
         // Default constructor
     }
 
-    public Flight(String destination, Address location, String duration, int flightNumber, String takeoffNumber, String time) {
-        this.destination = destination;
-        this.location = location;
+    public Flight(String from, String to, String fromAirport, String toAirport , String duration, int flightNumber, String time, String price) {
+        //this.destination = destination;
+        this.fromCity = from;
+        this.toCity = to;
+        this.fromAirport = fromAirport;
+        this.toAirport = toAirport;
+        //this.location = location;
         this.duration = duration;
         this.flightNumber = flightNumber;
-        this.takeoffNumber = takeoffNumber;
+        //this.takeoffNumber = takeoffNumber;
         this.timeOfFlight = time;
+        this.price = price;
     }
 
     // Getter and setter methods
+    public String getDeparture() {
+        return fromCity;
+    }
     public String getDestination() {
-        return destination;
+        return toCity;
+    }
+
+    public String getDepartureAirport() {
+        return fromAirport;
+    }
+    public String getDestinationAirport() {
+        return toAirport;
+    }
+
+    public void setDeparture(String departure) {
+        this.fromCity = departure;
     }
 
     public void setDestination(String destination) {
-        this.destination = destination;
+        this.toCity = destination;
     }
 
-    public Address getLocation() {
-        return location;
-    }
+    // public Address getLocation() {
+    //     return location;
+    // }
 
-    public void setLocation(Address location) {
-        this.location = location;
-    }
+    // public void setLocation(Address location) {
+    //     this.location = location;
+    // }
 
     public String getDuration() {
         return duration;
@@ -52,35 +75,40 @@ public class Flight {
         this.duration = duration;
     }
 
-    public int getFlightNumber() {
-        return flightNumber;
-    }
+    //AUTO GENERATED IN DATABASE
+    // public int getFlightNumber() {
+    //     return flightNumber;
+    // }
 
-    public void setFlightNumber(int flightNumber) {
-        this.flightNumber = flightNumber;
-    }
+    // public void setFlightNumber(int flightNumber) {
+    //     this.flightNumber = flightNumber;
+    // }
 
-    public String getTakeoffNumber() {
-        return takeoffNumber;
-    }
+    // public String getTakeoffNumber() {
+    //     return takeoffNumber;
+    // }
 
-    public void setTakeoffNumber(String takeoffNumber) {
-        this.takeoffNumber = takeoffNumber;
-    }
+    // public void setTakeoffNumber(String takeoffNumber) {
+    //     this.takeoffNumber = takeoffNumber;
+    // }
 
     public void printFlightDetails() {
         System.out.println("Flight Details:");
-        System.out.println("Destination: " + destination);
-        System.out.println("Location: " + location.getAddress());
+        System.out.println("Departure: " + fromCity);
+        System.out.println("From Airport: " + fromAirport);
+        System.out.println("Destination: " + toCity);
+        System.out.println("To Airport: " + toAirport);
+        //System.out.println("Location: " + location.getAddress());
         System.out.println("Duration: " + duration);
         System.out.println("Flight Number: " + flightNumber);
-        System.out.println("Takeoff Number: " + takeoffNumber);
+       // System.out.println("Takeoff Number: " + takeoffNumber);
         System.out.print("Time of flight: " + timeOfFlight);
+        System.out.println("Price: " + this.price);
     }
 
     public String getflightDetailsString(){
-        String flightDetailsString = "Flight Details:\n Destination: " + this.destination + "\nLocation: " + this.location.getAddress() + "\nDuration: "
-        + this.duration + "\nFlight Number: " + this.flightNumber + "\nTakeoff Number: " + this.takeoffNumber + "\nTIme of flight: " + this.timeOfFlight + "\n";
+        String flightDetailsString = "Flight Details:\n Departure: " + fromCity +  "\nDestination: " + this.toCity + "\nDeparture Airport: " + this.fromAirport +
+                                        "\nDestination Airport: "  + toAirport + "\nDuration: " + this.duration + "\nFlight Number: " + this.flightNumber + "\nTIme of flight: " + this.timeOfFlight + "\nPrice: " + this.price;
 
         return flightDetailsString;
     }
--- src/flight_system.sql
+++ src/flight_system.sql
@@ -15,14 +15,71 @@ CREATE TABLE REGISTERED_USERS (
     primary key (UserID)
 );
 
+ALTER TABLE REGISTERED_USERS AUTO_INCREMENT = 100;
+
 INSERT INTO REGISTERED_USERS (FirstName, LastName, Email, Pw, FullAdress)
 VALUES
 ('Jan', 'Petallo', 'jp@email.com', 'ensf480', '2500 University Drive NW Calgary Alberta Canada T2N 1N4'),
 ('Anita', 'John', 'jp@email.com', 'ensf480', '2501 University Avenue NW Calgary Alberta Canada T2N 1N4');
 
+
+DROP TABLE IF EXISTS FLIGHTS;
+CREATE TABLE FLIGHTS (
+    FlightNumber        int not null AUTO_INCREMENT,
+    Departure           varchar(50),
+    Destination         varchar(50),
+    DepartureAirport    varchar(50),
+    DestinationAirport  varchar(50),
+    Duration            varchar(25),
+    FlightTime          varchar(50),
+    Price               int,
+    primary key (FlightNumber)
+);
+
+ALTER TABLE FLIGHTS AUTO_INCREMENT = 1000;
+
+INSERT INTO FLIGHTS (Departure, Destination, DepartureAirport, DestinationAirport, Duration, FlightTime, Price)
+VALUES
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 41m', 'Nov 29, 2023 7:50 pm–8:31 pm', 201 ),
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 29m', 'Nov 30, 2023 9:15 pm – 11:44 pm', 167),
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 35m', 'Dec 01, 2023 2:55 pm – 5:30 pm', 175 ),
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 25m', 'Dec 02, 2023 8:40 pm – 11:05 pm', 182),
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 31m', 'Dec 03, 2023 7:30 am – 10:01 am', 205),
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 29m', 'Dec 04, 2023 8:40 pm – 11:05 pm', 172),
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 45m', 'Dec 05, 2023 6:15 pm – 7:00 pm', 158),
+('Calgary', 'Vancouver', 'YYC', 'YVR', '1h 35m', 'Dec 06, 2023 8:20 pm – 10:55 pm', 163),
+
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 44m', 'Nov 29, 2023 1:05 am – 6:49 am', 214 ),
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 55m', 'Nov 30, 2023 5:05 pm – 11:00 pm', 241),
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 55m', 'Dec 01, 2023 2:20 pm – 8:15 pm ', 243 ),
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 48m', 'Dec 02, 2023 6:55 am – 12:53 pm', 255),
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 44m', 'Dec 03, 2023 1:05 am – 6:49 am', 214 ),
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 55m', 'Dec 04, 2023 5:05 pm – 11:00 pm', 241),
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 55m', 'Dec 05, 2023 2:20 pm – 8:15 pm ', 243 ),
+('Calgary', 'Toronto', 'YYC', 'YYZ', '3h 48m', 'Dec 06, 2023 6:55 am – 12:53 pm', 255),
+
+('Calgary', 'Montreal', 'YYC', 'YUL', '4h 06m', 'Dec 01, 2023 7:00 am – 1:06 pm ', 237 ),
+('Calgary', 'Montreal', 'YYC', 'YUL', '4h 09m', 'Dec 02, 2023 9:10 am – 3:19 pm', 597),
+('Calgary', 'Montreal', 'YYC', 'YUL', '4h 19m', 'Dec 03, 2023 12:10 pm – 6:29 pm', 946 ),
+('Calgary', 'Montreal', 'YYC', 'YUL', '8h 45m', 'Dec 04, 2023 7:30 am – 6:15 pm', 589),
+('Calgary', 'Montreal', 'YYC', 'YUL', '4h 06m', 'Dec 05, 2023 7:00 am – 1:06 pm ', 237 ),
+('Calgary', 'Montreal', 'YYC', 'YUL', '4h 09m', 'Dec 06, 2023 9:10 am – 3:19 pm', 597),
+('Calgary', 'Montreal', 'YYC', 'YUL', '4h 19m', 'Dec 07, 2023 12:10 pm – 6:29 pm', 946 ),
+('Calgary', 'Montreal', 'YYC', 'YUL', '8h 45m', 'Dec 08, 2023 7:30 am – 6:15 pm', 589),
+
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 50m', 'Dec 01, 2023 9:50 am – 10:40 am', 171 ),
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 59m', 'Dec 02, 2023 11:05 pm – 12:04 am', 250),
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 57m', 'Dec 03, 2023 8:00 pm – 8:57 pm', 371 ),
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 54m', 'Dec 04, 2023 2:30 pm – 3:24 pm', 354),
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 50m', 'Dec 05, 2023 9:50 am – 10:40 am', 171 ),
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 59m', 'Dec 06, 2023 11:05 pm – 12:04 am', 250),
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 57m', 'Dec 07, 2023 8:00 pm – 8:57 pm', 371 ),
+('Calgary', 'Edmonton', 'YYC', 'YEG', '0h 54m', 'Dec 08, 2023 2:30 pm – 3:24 pm', 354);
+
 USE FLIGHT_SYSTEM;
 SHOW TABLES;
 SELECT * FROM REGISTERED_USERS;
+SELECT * FROM FLIGHTS;
 
 
 
