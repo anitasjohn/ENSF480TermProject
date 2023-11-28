2efef0b2855e2fc0310fb073f09629e7705857d2
janpetallo <101074771+janpetallo@users.noreply.github.com>
2023-11-28 10:25:18 -0700

    Added agentLogin()

--- src/AccessDatabase.java
+++ src/AccessDatabase.java
@@ -174,6 +174,39 @@ public class AccessDatabase {
         return true;
     }
 
+    // airline agent login
+    public boolean agentLogin(String email, String pw){
+        try {
+            String query = "SELECT * AIRLINE_AGENTS WHERE Email = ? AND Pw = ?";
+            PreparedStatement myStmt = dbConnect.prepareStatement(query);
+
+            myStmt.setString(1, email);
+            myStmt.setString(2, pw);
+
+            results = myStmt.executeQuery();
+
+            myStmt.close();
+
+            if(!results.next()){ // if no existing user
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }finally {
+            try {
+                 if(dbConnect != null){
+                    results.deleteRow(); // double check if this is correct
+                    dbConnect.close();
+                 }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return true; // if there exists an agent with the same login credentials
+    }
+
     // get all flights for browsing given a destination city
     public ArrayList<Flight> fetchFLights(String destination){
         ArrayList<Flight> flights = new ArrayList<Flight>();
