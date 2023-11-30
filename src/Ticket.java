c8ed33590d7be43616bcf797009f102e1aea786a
janpetallo <101074771+janpetallo@users.noreply.github.com>
2023-11-29 19:05:30 -0700

    Added database access and a new constructor

--- src/Ticket.java
+++ src/Ticket.java
@@ -11,9 +11,25 @@ public class Ticket {
     private String holder;
     private String seatNum;
     private boolean insured;
+    private int flightNum;
 
+    AccessDatabase database = AccessDatabase.getOnlyInstance();
+
+    // used when generating a NEW ticket
     public Ticket(Flight flight, String FName, String seat, boolean insured) {
         this.flight = flight;
+        this.holder = FName;
+        this.seatNum = seat;
+        this.insured = insured;
+        // insert new ticket into the database
+        database.initializeConnection();
+        database.insertNewPassenger(flight.getFlightNumber(), FName, flightNum, insured);
+    }
+
+    // only used by fetchTicketHolders() in AccessDatabase.java
+    // only used when fetching the ticket holders for a specific flight 
+    public Ticket(int flightNum, String FName, String seat, boolean insured) {
+        this.flightNum = flightNum;
         this.holder = FName;
         this.seatNum = seat;
         this.insured = insured;
